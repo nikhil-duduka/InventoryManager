31321aebd0d7f79468447f03e84192969bf7acad
yeshwanth1524 <yeshw.yeshwanth15@gmail.com>
2025-01-04 09:38:27 +0530

    express added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ const PORT = 5000;
 
 app.use(express.json());
 
+app.get("/",(req,res) => {
+    res.status(200).send("Server Check");
+});
 
 app.listen(PORT, () => {
         console.log('server is running at port',PORT);
